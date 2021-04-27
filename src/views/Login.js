c0fcefd80bdaf990a0e8b063798bb34327149f81
Michael <mmcshinsky@gmail.com>
2021-04-26 22:02:59 -0600

    auth changes

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,54 +1,130 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import firebase from "@/firebase";
-import "firebase/auth";
-import "firebase/firestore";
-import { useAuth } from '@/services/context/auth';
+import { Link, useHistory } from "react-router-dom";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  Alert,
+} from "reactstrap";
 
-export const Login = () => {
-    const { setUser } = useAuth();
-    const history = useHistory();
-    const [state, setState] = useState({
-        email: "",
-        password: ""
-    });
-    const handleClick = () => {
-      history.push("/register");
-    }
-    const handleChange = (event) => {
-      event.persist();
-      setState(state => ({
-          ...state,
-          [event.target.name]: event.target.value
+import { useAuth } from "@/services/context/auth";
+import firebase from "@/firebase";
+
+const MESSAGE = {
+  text: '',
+  type: null,
+};
+
+export function Login() {
+  const [state, setState] = useState({
+    email: '',
+    password: '',
+    message: { ...MESSAGE },
+    isSubmitting: false,
+  });
+  const { setUser } = useAuth();
+  const history = useHistory();
+
+  function _handleChange (e) {
+    e.persist();
+    setState(prevState => ({ ...prevState, message: { ...MESSAGE }, [e.target.name]: e.target.value }));
+  }
+
+  function _handleSubmit(event) {
+    event.preventDefault();
+
+    setState(prevState => ({
+      ...prevState,
+      isSubmitting: true,
     }));
-   }
-   const handleSubmit = (event) => {
-      event.preventDefault();
-      firebase
+
+    firebase
       .auth()
       .signInWithEmailAndPassword(state.email, state.password)
-      .then(res => {
+      .then((res) => {
         setUser(res);
-        console.log(res, 'res')
         history.push("/admin");
-     })
-      .catch(error => {
-      console.log(error.message);
-      alert(error.message);
-     });
-   }
-  
+      })
+      .catch((error) => {
+        setState(prevState => ({
+          ...prevState,
+          isSubmitting: false,
+          message: {
+            text: error.message,
+            type: 'warning',
+          },
+        }));
+      });
+  }
+
   return (
-     <div style={{textAlign: 'center'}}>
-     <h1>Login</h1>
-     <form onSubmit={handleSubmit}>
-      <input type="text" name="email" value={state.email}     placeholder="Enter your Email" onChange={handleChange} /><br /><br />
-      <input type="password" name="password" value={state.password} placeholder="Enter your Password" onChange={handleChange} /><br /><br />
-      <button>Login</button>
-      <p>Not logged in yet?</p>
-      <button onClick={handleClick}>SignUp</button>
-     </form>
-   </div>
+    <div className="flex-fill d-flex w-100 align-items-center">
+      <Container>
+        <Row>
+          <Col
+            xs={{ size: 10, offset: 2 }}
+            sm={{ size: 8, offset: 2 }}
+            md={{ size: 6 }}
+            lg={{ size: 4, offset: 4 }}
+            className="mx-auto"
+          >
+            <h1 className="text-center">Login</h1>
+            {state.message.type && (
+              <Alert color={state.message.type}>{state.message.text}</Alert>
+            )}
+            <Form onSubmit={_handleSubmit}>
+              <FormGroup>
+                <Label for="loginEmail">Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  id="loginEmail"
+                  required
+                  autoComplete="email"
+                  placeholder="email@domain.com"
+                  value={state.email}
+                  onChange={_handleChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="loginPassword">Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  id="loginPassword"
+                  required
+                  autoComplete="password"
+                  value={state.password}
+                  onChange={_handleChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Button
+                  color="primary"
+                  block
+                  className="shadow"
+                  disabled={state.isSubmitting}
+                >
+                  {state.isSubmitting && (
+                    <i className="las la-spinner la-spin"></i>
+                  )}
+                  Login
+                </Button>
+              </FormGroup>
+              <FormGroup className="d-flex justify-content-between">
+                <Link to="/register">Register</Link>
+                <Link to="/forgot-password">Forgot Password</Link>
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
--- src/views/Register.js
+++ src/views/Register.js
@@ -1,69 +1,291 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Alert,
+} from "reactstrap";
+import { useAuth } from "@/services/context/auth";
 import firebase from "@/firebase";
-import "firebase/auth";
-import "firebase/firestore";
-import { useAuth } from '@/services/context/auth';
+import { Validation } from '@/components';
 
-export const Register = () => {
+const VALIDATIONS = {
+  minLength: false,
+  oneLower: false,
+  oneUpper: false,
+  oneNumber: false,
+  oneSpecial: false,
+};
+const MIN_LENGTH = 8;
+const MESSAGE = {
+  text: "",
+  type: null,
+};
+
+export function Register() {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    passwordVisible: false,
+    confirmPassword: "",
+    confirmPasswordVisible: false,
+    passwordValidations: VALIDATIONS,
+    message: MESSAGE,
+    isSubmitting: false,
+  });
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+  const [message, setMessage] = useState({ ...MESSAGE });
+
   const { setUser } = useAuth();
-   const [values, setValues] = useState({
-      username: "",
-      email: "",
-      password: "",
-      phone: ""
-    });
- 
-    const history = useHistory();
-    const handleClick = () => {
-       history.push("/login")
-   }
-   const handleChange = (event) => {
-      event.persist();
-      setValues(values => ({
-        ...values,
-        [event.target.name]: event.target.value
+  const history = useHistory();
+
+  function _handleChange(e) {
+    e.persist();
+    if (e.target.name === "password") {
+      _validatePassword(e.target.value);
+    }
+    setMessage({ ...MESSAGE });
+    setState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
     }));
-   }
-    const handleSubmit = (event) => {
-      event?.preventDefault();
-      console.log(values, 'values');
-      firebase
+  }
+
+  function _validatePassword(value) {
+    let validations = { ...state.passwordValidations };
+
+    validations.minLength = !(value.length < MIN_LENGTH);
+    validations.oneLower = !!/[a-z]/.test(value);
+    validations.oneUpper = !!/[A-Z]/.test(value);
+    validations.oneNumber = !!/[0-9]/.test(value);
+    validations.oneSpecial = !!/[!@#$%^&*()_+]/.test(value);
+
+    setState((prevState) => ({
+      ...prevState,
+      passwordValidations: validations,
+    }));
+  }
+
+  function _togglePassword() {
+    setPasswordVisible(!passwordVisible);
+  }
+
+  function _togglePasswordConfirm() {
+    setConfirmPasswordVisible(!confirmPasswordVisible);
+  }
+
+  function _handleSubmit(e) {
+    e.preventDefault();
+    setState((prevState) => ({ ...prevState, isSubmitting: true }));
+
+    let valid = true;
+
+    Object.keys(state.passwordValidations).forEach((key) => {
+      if (state.passwordValidations[key] === false) {
+        valid = false;
+      }
+    });
+
+    if (!valid) {
+      setState((prevState) => ({
+        ...prevState,
+        message: {
+          text: "Missing required fields.",
+          type: "info",
+        },
+        isSubmitting: false,
+      }));
+      return;
+    }
+
+    firebase
       .auth()
-      .createUserWithEmailAndPassword(values.email, values.password)
-      .then((userCredential) => {
-          setUser(userCredential);
-          const db = firebase.firestore();
-          db.collection("Users")
-           .doc(userCredential.user.uid)
-           .set({
-              email: values.email,
-              username: values.username,
-              phone: values.phone
-         })
-          .then(() => {
-              console.log('ok');
-              history.push("/admin");
-         })
-             .catch(error => {
-                 console.log(error.message);
-                 alert(error.message);
-         });
-    })
-  } 
+      .createUserWithEmailAndPassword(state.email, state.password )
+      .then((res) => {
+        setState((prevState) => ({
+          ...prevState,
+          email: "",
+          password: "",
+          confirmPassword: "",
+          message: {
+            text: "Success! An email has been sent to you for verification.",
+            type: "info",
+          },
+          isSubmitting: false,
+        }));
+        return res;
+      })
+      .then((res) => {
+        console.log('res', res)
+        setUser(res)
+        setState((prevState) => ({
+          ...prevState,
+          isSubmitting: false,
+        }));
+
+        setTimeout(() => {
+          return res.user.sendEmailVerification({
+            url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
+          });
+        }, 1000);
+      })
+      .catch((err) => {
+        setState((prevState) => ({
+          ...prevState,
+          message: {
+            text: err.message,
+            type: "warning",
+          },
+          isSubmitting: false,
+        }));
+      });
+  }
+
+  let confirmValid =
+    state.password !== state.confirmPassword &&
+    state.confirmPassword.length > 0 &&
+    state.password.length > 0;
+
   return (
-     <div style={{textAlign: 'center'}}>
-     <h1>Sign Up</h1>
-     <form onSubmit={handleSubmit}>
-        <input type="text" name="username" placeholder="Username" onChange={handleChange} /><br /><br />
-        <input type="text" name="phone" placeholder="Phone" onChange={handleChange}/><br /><br />
-        <input type="text" name="email" placeholder="Enter your Email" onChange={handleChange}/><br /><br />
-        <input type="password" name="password" placeholder="Enter your Password" onChange={handleChange}/><br /><br />
-        <button type="submit">Sign Up</button>
-        <p>Already have account?</p>
-        <button onClick={handleClick}>Login</button>
-      </form>
-     </div>
-   );
+    <div className="flex-fill d-flex w-100 align-items-center">
+      <Container>
+        <Row>
+          <Col
+            xs={{ size: 10, offset: 2 }}
+            sm={{ size: 8, offset: 2 }}
+            md={{ size: 6 }}
+            lg={{ size: 4, offset: 4 }}
+            className="mx-auto"
+          >
+            <h1 className="text-center">Register</h1>
+            {message.type && <Alert color={message.type}>{message.text}</Alert>}
+            <Form onSubmit={_handleSubmit}>
+              <FormGroup>
+                <Label for="loginEmail">Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  id="loginEmail"
+                  required
+                  placeholder="email@domain.com"
+                  value={state.email}
+                  onChange={_handleChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="loginPassword">Password</Label>
+                <InputGroup>
+                  <Input
+                    type={passwordVisible ? "text" : "password"}
+                    name="password"
+                    id="loginPassword"
+                    required
+                    value={state.password}
+                    onChange={_handleChange}
+                  />
+                  <InputGroupAddon
+                    addonType="prepend"
+                    onClick={_togglePassword}
+                  >
+                    <InputGroupText>
+                      <i
+                        className={`lar la-eye${
+                          passwordVisible ? "" : "-slash"
+                        }`}
+                      ></i>
+                    </InputGroupText>
+                  </InputGroupAddon>
+                </InputGroup>
+                {state.password && (
+                  <>
+                    <Validation
+                      valid={state.passwordValidations.minLength}
+                      message="Minimum of 8 characters"
+                    />
+                    <Validation
+                      valid={state.passwordValidations.oneLower}
+                      message="At least one lowercase letter [a-z]"
+                    />
+                    <Validation
+                      valid={state.passwordValidations.oneUpper}
+                      message="At least one uppercase letter [A-Z]"
+                    />
+                    <Validation
+                      valid={state.passwordValidations.oneNumber}
+                      message="At least one number [0-9]"
+                    />
+                    <Validation
+                      valid={state.passwordValidations.oneSpecial}
+                      message="At least one special character [!'#$%&\'()*+,-./:;<=>?@\\^`{|}~]'"
+                    />
+                  </>
+                )}
+              </FormGroup>
+              <FormGroup>
+                <Label for="loginConfirmPassword">Confirm Password</Label>
+                <InputGroup>
+                  <Input
+                    type={confirmPasswordVisible ? "text" : "password"}
+                    name="confirmPassword"
+                    id="loginConfirmPassword"
+                    required
+                    value={state.confirmPassword}
+                    onChange={_handleChange}
+                    invalid={confirmValid}
+                  />
+                  <InputGroupAddon
+                    addonType="prepend"
+                    onClick={_togglePasswordConfirm}
+                  >
+                    <InputGroupText>
+                      <i
+                        className={`lar la-eye${
+                          confirmPasswordVisible ? "" : "-slash"
+                        }`}
+                      ></i>
+                    </InputGroupText>
+                  </InputGroupAddon>
+                </InputGroup>
+                {confirmValid && (
+                  <Validation
+                    valid={!confirmValid}
+                    message="Passwords do not match"
+                  />
+                )}
+              </FormGroup>
+              <FormGroup>
+                <Button
+                  color="primary"
+                  block
+                  className="shadow"
+                  disabled={state.isSubmitting}
+                >
+                  {state.isSubmitting && (
+                    <i className="las la-spinner la-spin"></i>
+                  )}{" "}
+                  Register
+                </Button>
+              </FormGroup>
+              <FormGroup className="d-flex justify-content-between">
+                <Link to="/login">
+                  <i className="las la-arrow-left"></i> Back to Login
+                </Link>
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 export default Register;
